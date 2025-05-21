da18fe172ed79b9cfb75ae8afb20321c8cd88560
SaberHardy <samirsamado5@gmail.com>
2025-05-21 07:06:44 +0100

    use crontab to schedule the executions

---
+++ dags/dag_with_cron_expression.py
@@ -0,0 +1,21 @@
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+from airflow import DAG
+
+default_args = {
+    'owner': 'saber',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(dag_id="dag_with_cron_expression",
+         default_args=default_args,
+         start_date=datetime(2025, 5, 10, 2),
+         catchup=False,
+         schedule_interval="Tue,Sun") as dag:
+    task1 = BashOperator(task_id='cron_task',
+                         bash_command="echo this is new call")
+
+    task1

fb1512ffd1aab5257aef2f0d6402f3a375f38a86
SaberHardy <samirsamado5@gmail.com>
2025-05-21 08:58:42 +0100

    remove the schedule_interval

--- dags/create_postgres_data.py
+++ dags/create_postgres_data.py
@@ -1,8 +1,7 @@
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 from airflow import DAG
+import json
 
 default_args = {
     'owner': 'saber',
@@ -10,11 +9,11 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-with DAG(dag_id="dag_with_cron_expression",
+with DAG(dag_id="python_dag_operator_v2",
          default_args=default_args,
+         description='Small Description',
          start_date=datetime(2025, 5, 10, 2),
-         catchup=False) as dag:
-    task1 = BashOperator(task_id='cron_task',
-                         bash_command="echo this is new call")
-
-    task1
+         catchup=False,
+         schedule_interval=None) as dag:
+    pass
+# 1:11:02

5a32c4c6925dd3e5dd6f4382b272fc9709e2b8c4
SaberHardy <samirsamado5@gmail.com>
2025-05-22 17:12:53 +0100

    add postgres db

--- dags/create_postgres_data.py
+++ dags/create_postgres_data.py
@@ -1,7 +1,7 @@
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from airflow import DAG
-import json
 
 default_args = {
     'owner': 'saber',
@@ -9,11 +9,14 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-with DAG(dag_id="python_dag_operator_v2",
+with DAG(dag_id="dag_with_postgres_operator",
          default_args=default_args,
          description='Small Description',
          start_date=datetime(2025, 5, 10, 2),
          catchup=False,
          schedule_interval=None) as dag:
-    pass
-# 1:11:02
+    task1 = PostgresOperator(
+        task_id="create_postgres_table",
+        postgres_conn_id=""
+    )
+    task1
